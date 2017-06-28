8ff688d39fad56dbec459c7a1403f1a436773b45
keerthanakavitha <keerthuharsu@gmail.com>
2017-06-28 11:58:11 +0530

    Create Factorialnumber.java

---
+++ Factorialnumber.java
@@ -0,0 +1,13 @@
+public class Fact
+{
+public static void main(String[]args)
+{
+int factorial=1;
+Scanner s=new Scanner(System.in);
+System.out.println("enter the number");
+int n=s.nextInt();
+for(int i=1;i<=n;i++)
+{
+factorial=factrial*i;
+}
+System.out.println(" "+factorial);
