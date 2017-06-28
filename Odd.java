adb70ff44581846ed50bf215b011ea9d4bb3fedb
keerthanakavitha <keerthuharsu@gmail.com>
2017-06-28 14:33:52 +0530

    Create Odd.java

---
+++ Odd.java
@@ -0,0 +1,15 @@
+public class Odd
+{
+public static void main(String[]args)
+{
+int limit=10;
+System.out.println("print odd numbers between 1 and"+limit);
+for(int i=1;i<=10;i++)
+{
+if(i%2!=0)
+{
+System.out.println(i);
+}
+}
+}
+}
