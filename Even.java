1aa74743755b165f93a176169fc31a3551714b63
keerthanakavitha <keerthuharsu@gmail.com>
2017-06-28 14:28:09 +0530

    Create Even.java

---
+++ Even.java
@@ -0,0 +1,15 @@
+public class Even
+{
+public static void main(String[]args)
+{
+int limit=10;
+System.out.println("print even numbers between 1 and"+limit);
+for(int i=1;i<=10;i++)
+{
+if(i%2==0)
+{
+System.out.println(i+ " ");
+}
+}
+}
+}
