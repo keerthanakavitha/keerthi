de7d152dab256b5c05eb2b40c4deb6175bccb756
keerthanakavitha <keerthuharsu@gmail.com>
2017-06-28 10:04:32 +0530

    Create Stringreverse.java

---
+++ Stringreverse.java
@@ -0,0 +1,7 @@
+public class Stringreverse
+{
+public static void main(String[]args)
+{
+String str="keerthu";
+String reverse=new StringBuffer(str).reverse().toString();
+System.out.println("string reverse is:"+reverse);
