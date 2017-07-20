6389f967643d2b39e6b075d2b8115e8a9de1acee
keerthanakavitha <keerthuharsu@gmail.com>
2017-07-20 12:01:22 +0530

    Create Val.java

---
+++ Val.java
@@ -0,0 +1,9 @@
+Class Values
+{
+public static void main(String args[]) {
+        int i;
+        for(i = 0; i < 26; i++){
+            System.out.println((char)('A'+i) + " --> " + ('A'+i));
+            }
+            }
+}
