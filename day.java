b4986eb79bdf82fd64f7512f73af43dbbb974ffc
keerthanakavitha <keerthuharsu@gmail.com>
2017-07-26 15:24:17 +0530

    Create day.java

---
+++ day.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+class Day
+{
+public static void main(String[]args)
+{
+int holiday=0;
+int workingday=0;
+if(workingday)
+{
+System.out.println("true");
+}
+else
+{
+System.out.println("false");
+}
+}
+}
